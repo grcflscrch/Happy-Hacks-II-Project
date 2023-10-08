93a8a6d6f851b2b2f3b3d77f756fe3977b06e26e
grcflscrch <147227963+grcflscrch@users.noreply.github.com>
2023-10-08 22:37:18 +0000

    Changes to be committed: 	modified:   assignments.html 	new file:   base.html 	modified:   database files/loginfo files/loginfoFunct.js 	modified:   index.html 	modified:   teachers.html

--- database files/loginfo files/loginfoFunct.js
+++ database files/loginfo files/loginfoFunct.js
@@ -1,1 +1,14 @@
+document.getElementById("uname").ariaValueMax;
+document.getElementById("pword").ariaValueMax;
 
+var exi = JSON.stringify(loginfo).includes(uname) --> true
+if (exi === true) {
+    var lo = JSON.stringify(loginfo).includes(pword) -- tr.ue
+if (lo === true) {
+    console.log('Login Successful')
+} else {
+    console.log('Incorrect Password')
+}
+} else {
+    console.log('No User Found')
+}
